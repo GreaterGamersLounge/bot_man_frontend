5d1106c7c7d503d588d0313a9353939cb6d2d371
AdamKogut <akogut@purdue.edu>
2020-01-06 23:36:20 -0500

    Uncomplicate navigation

--- src/components/NavigationBar/Tabs.js
+++ src/components/NavigationBar/Tabs.js
@@ -9,32 +9,12 @@ const StyledTabs = withStyles(theme => ({
   }
 }))(props=><Tab {...props}/>)
 
-
-function getTabValue(){
-  let location=window.location.pathname;
-  let tabValue=0;
-  if(location.indexOf('/users')===0){
-    tabValue=1;
-  } else {
-    tabValue=0;
-  }
-  return tabValue;
-}
-
 function MyTabs() {
-  const [value, setValue] = React.useState(0);
-  let tempValue=getTabValue();
-  if(tempValue!==value){
-    setValue(tempValue);
-  }
+  let location=window.location.pathname;
+  const [value, setValue] = React.useState(location);
 
-  const changeTabValue=(event,newValue)=>{
+  const changeTabValue=(_,newValue)=>{
     setValue(newValue);
-    if(newValue===0){
-      History.push('/');
-    } else if(newValue===1){
-      History.push('/users');
-    }
   }
 
   return (
@@ -44,8 +24,8 @@ function MyTabs() {
       style={{ marginTop: "auto", marginBottom: 0 }}
       scrollButtons="auto"
     >
-      <StyledTabs label="Home"/>
-      <StyledTabs label="Users" />
+      <StyledTabs to='/' value='/' label="Home"/>
+      <StyledTabs to='/users' value='/users' label="Users" />
       <StyledTabs label="Home3" />
     </Tabs>
   );
