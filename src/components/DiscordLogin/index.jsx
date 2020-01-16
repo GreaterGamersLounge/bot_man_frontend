b36ea5742516e4e7d2b357a2eed07f7b641cd65a
dracco1993 <dracco1993@gmail.com>
2020-01-15 23:58:40 -0500

    IT'S WORKING

--- src/components/DiscordLogin/index.jsx
+++ src/components/DiscordLogin/index.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import { Skeleton } from "@material-ui/lab";
 import { Button } from "@material-ui/core";
 import styled from "styled-components";
 import DiscordLogo from "./Discord-Logo+Wordmark-Color.svg";
-import { Link, withRouter } from "react-router-dom";
-import { URL } from "../config";
+import { API_URL } from "../config";
 
 const DiscordButton = styled(Button)`
   background-color: #7289da !important;
@@ -13,38 +11,18 @@ const DiscordButton = styled(Button)`
 `;
 
 class DiscordLogin extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   const placeholderList = [];
-  //   for (let i = 0; i < 4; i += 1) {
-  //     placeholderList.push(
-  //       <StyledSkeleton key={i} variant="rect" animation="wave" height={100} />
-  //     );
-  //   }
-  //   this.state = {
-  //     displayList: [],
-  //     placeholderList
-  //   };
-  // }
-
   handleLogin = e => {
-    console.log(e);
     e.preventDefault();
-    // this.props.history.push("/dashboard");
-    window.location.href = `${URL}/auth/discord`;
-    // window.location.href = "/auth/discord";
-    // window.location.replace("/auth/discord");
+    window.location.href = `${API_URL}/users/auth/discord`;
   };
 
   render() {
     return (
-      // <Link to="/auth/discord">
       <DiscordButton onClick={this.handleLogin}>
         <img height={47} src={DiscordLogo} alt="Discord Logo" />
       </DiscordButton>
-      // </Link>
     );
   }
 }
 
-export default withRouter(DiscordLogin);
+export default DiscordLogin;
